7c5ab3c0bb33cf2b639abe6ae740fb04f2da7bf2
xiangcitoZ <pzxiang1@gmail.com>
2023-04-14 14:15:47 +0200

    Add project files.

---
+++ MvcCoreApiCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreApiCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
